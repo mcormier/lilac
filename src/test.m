2d78117b38bb1a37b260e461aa29f50d0b5b3e4d
Matthieu Cormier <mcormier@cs.dal.ca>
2009-09-01 09:39:42 -0300

    Updated test code

--- src/test.m
+++ src/test.m
@@ -1,12 +1,13 @@
 
 const char* value = "\"C:\\Users\\Matthieu.Cormier\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe\" -- \"%1\"";
 
-// CFStringCreateArrayBySeparatingStrings
+// TODO -- get @"" notation working..
 
 #import <objc/Object.h>
 #import <objc/NXConstStr.h>
 #import "NSString.h"
 #import "NSArray.h"
+#import "NSConstantString.h"
 
 int main ( int argc, const char * argv[] ) {
 
@@ -19,8 +20,17 @@ int main ( int argc, const char * argv[] ) {
   int count = [array count];
   printf("the count is %i \n", count);
 
+  int i;
+  for (i = 0; i < [array count]; i++ ) {
+    NSString* blah = [array objectAtIndex:i];
+    [blah print];  
+  }
+
   [regValue release];
   [sepValue release];
+
+  NSConstantString *cnstString = @"constant String";
+  printf("The length is %i \n", [cnstString length]);
 
   return 0;
 }
